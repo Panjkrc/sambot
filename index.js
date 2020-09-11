80ea90a5eae0691ec4b066c313eb344531fc278e
Panjkrc <panjkrc.mihael@gmail.com>
2020-09-11 22:47:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -70,24 +70,25 @@ client.on('message', message => {
 client.on('error', console.error);
 
 client.on('messageReactionAdd', async (reaction, user) => {
-	// When we receive a reaction we check if the reaction is partial or not
-	if (reaction.partial) {
-		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-		try {
-			await reaction.fetch();
+	if (reaction.message.member.guild.me.hasPermission('KICK_MEMBERS')) {
+		// When we receive a reaction we check if the reaction is partial or not
+		if (reaction.partial) {
+			// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
+			try {
+				await reaction.fetch();
 
+			}
+			catch (error) {
+				console.log('Something went wrong when fetching the message: ', error);
+				// Return as `reaction.message.author` may be undefined/null
+				return;
+			}
 		}
-		catch (error) {
-			console.log('Something went wrong when fetching the message: ', error);
-			// Return as `reaction.message.author` may be undefined/null
-			return;
+		if (reaction.emoji != null && reaction.emoji.id == '674859298131542029') {
+			const messageContent = reaction.message;
+			reaction.message.delete();
+			reaction.message.channel.send(`Message sent by **${reaction.message.author.username}**:\n\`\`\`cpp\n${messageContent.content} \`\`\` `);
 		}
-	}
-	console.log(reaction.emoji.id);
-	if (reaction.emoji != null && reaction.emoji.id == '674859298131542029') {
-		const messageContent = reaction.message;
-		reaction.message.delete();
-		reaction.message.channel.send(`@${user.tag} please upload your code to PasteBin`);
 	}
 });
 
